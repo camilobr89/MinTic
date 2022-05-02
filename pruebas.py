410424eb28b76a0468bb7d3c2e558863daf0e56c
camilobr89 <camilobr89@hotmail.com>
2022-05-02 11:50:41 -0500

    ejercicio en clase

--- pruebas.py
+++ pruebas.py
@@ -1,24 +1,27 @@
-def deuda(b, p, l, h):
-    PANES = 300
-    LECHE = 3300
-    HUEVOS = 350
+# Se le solicita elaborar de manera individual un programa en python que: 
+# 1- Cuantos productos va a facturar 2- Debe mandar ese elemento a una función donde va a solicitar: - Nombre del articulo - Precio del articulo sin IVA - Cantidad - Porcentaje del IVA 3- Calcula por cada articulo - El subtotal sin Iva - El valor del IVA - Total con IVA 4- Adicionalmente debe acumular todos los subtotales, todos los valores del IVA y todos los totales de los productos. 5- El gerente comercial buscando incremen
+# 5- El gerente comercial buscando incrementar las ventas ofrece: - Si el total de la factura supera 5 millones se le da el 10% al subtotal - Si el total de la factura supera 2 millones se le da el 3% al subtotal - Si el total de la factura supera 1 millones se le da el 1% al subtotal - Para otro valor no se da descuento 
 
-    return b - (p * PANES + l * LECHE + h * HUEVOS)
+def facturacion(nombre, precio, cantidad, iva):
+    subtotal = precio * cantidad
+    iva = subtotal * iva
+    total = subtotal + iva
+    return subtotal, iva, total
 
-    
+    while True:
+        nombre = input("Ingrese el nombre del producto: ")
+        precio = float(input("Ingrese el precio del producto: "))
+        cantidad = int(input("Ingrese la cantidad de productos: "))
+        iva = float(input("Ingrese el porcentaje del IVA: "))
+        subtotal, iva, total = facturacion(nombre, precio, cantidad, iva)
+        print("El subtotal es: ", subtotal)
+        print("El valor del IVA es: ", iva)
+        print("El total es: ", total)
+        print("Desea continuar? S/N")
+        opcion = input()
+        if opcion == "N":
+            break
+        else:
+            continue
 
-def run():
-    b = int(input("Ingrese el valor del billete: "+ "$"))
-    p = int(input("Ingrese la cantidad de panes: "))
-    l = int(input("Ingrese la cantidad de leche: "))
-    h = int(input("Ingrese la cantidad de huevos: "))
 
-    if deuda(b, p, l, h) < 0:
-        print("Usted debe devolver $%d pesos" % abs(deuda(b, p, l, h)))
-    else:
-        print("Sus vueltas son: $%d pesos" % deuda(b, p, l, h))
-
-
-
-if __name__ == "__main__":
-    run()
