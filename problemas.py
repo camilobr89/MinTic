bec1fae8154f94da95eb590417b65413af285501
camilobr89 <camilobr89@hotmail.com>
2022-05-02 21:19:54 -0500

    Ejercicio terminado

--- problemas.py
+++ problemas.py
@@ -57,26 +57,26 @@
 # # programa que me diga las vueltas (o lo que quedo debiendo) cuando
 # # me da un billete de B pesos.
 
-# def deuda(b, p, l, h):
-#     PANES = 300
-#     LECHE = 3300
-#     HUEVOS = 350
+def deuda(b, p, l, h):
+    PANES = 300
+    LECHE = 3300
+    HUEVOS = 350
 
-#     return b - (p * PANES + l * LECHE + h * HUEVOS)
+    return b - (p * PANES + l * LECHE + h * HUEVOS)
 
-# def run():
-#     b = int(input("Ingrese el valor del billete: " + "$"))
-#     p = int(input("Ingrese la cantidad de panes: "))
-#     l = int(input("Ingrese la cantidad de leche: "))
-#     h = int(input("Ingrese la cantidad de huevos: "))
+def run():
+    b = int(input("Ingrese el valor del billete: " + "$"))
+    p = int(input("Ingrese la cantidad de panes: "))
+    l = int(input("Ingrese la cantidad de leche: "))
+    h = int(input("Ingrese la cantidad de huevos: "))
 
-#     if deuda(b, p, l, h) < 0:
-#         print("Usted debe devolver $%d pesos" % abs(deuda(b, p, l, h)))
-#     else:
-#         print("Sus vueltas son: $%d pesos" % deuda(b, p, l, h))
+    if deuda(b, p, l, h) < 0:
+        print("Usted debe devolver $%d pesos" % abs(deuda(b, p, l, h)))
+    else:
+        print("Sus vueltas son: $%d pesos" % deuda(b, p, l, h))
 
-# if __name__ == "__main__":
-#     run()
+if __name__ == "__main__":
+    run()
 
 ###############################################################################
 
--- pruebas.py
+++ pruebas.py
@@ -3,42 +3,87 @@
 # 5- El gerente comercial buscando incrementar las ventas ofrece: - Si el total de la factura supera 5 millones se le da el 10% al subtotal - Si el total de la factura supera 2 millones se le da el 3% al subtotal - Si el total de la factura supera 1 millones se le da el 1% al subtotal - Para otro valor no se da descuento 
 
 
-subTotal = 0
-i = 0
+# subTotal = 0
+# i = 0
 
-productos = int(input("Cuantos productos va a facturar: "))
+# productos = int(input("Cuantos productos va a facturar: "))
 
-while i < productos:
-    nombre = input("Nombre del articulo: ")
-    precio = float(input("Precio del articulo sin IVA: "))
-    cantidad = int(input("Cantidad: "))
-    porcentaje = float(input("Porcentaje del IVA: "))
-    subtotal = precio * cantidad
+# while i < productos:
+    # nombre = input("Nombre del articulo: ")
+    # precio = float(input("Precio del articulo sin IVA: "))
+    # cantidad = int(input("Cantidad: "))
+    # porcentaje = float(input("Porcentaje del IVA: "))
+    # subtotal = precio * cantidad
         
-    subTotal += subtotal
+#     subTotal += subtotal
+#     iva = subTotal * porcentaje
+#     total = subTotal + iva
+#     i += 1
+#     print("Subtotal: $%d" %subTotal)
+#     print("Total con IVA: $%d" %total)
+#     print("IVA: $%d" %iva, end="\n\n")
+
+# if total > 5000000:
+#     total -= subTotal * 0.10
+#     print("El total de la factura con un descuento del 10 porciento es: $%d" %total)
+# elif total > 2000000:
+#     total -= subTotal * 0.03
+#     print("El total de la factura con un descuento del 3 porciento es: $%d" %total)
+# elif total > 1000000:
+#     total -= subTotal * 0.01
+#     print("El total de la factura con un descuento del 1 porciento es: $%d" %total)
+# else:
+#     total = total
+#     print("El total de la factura es: $%d" %total)
+
+###############################################################################
+
+
+def facturacion(precio, cantidad, porcentaje):
+    subTotal = precio * cantidad
     iva = subTotal * porcentaje
     total = subTotal + iva
-    i += 1
-    print("Subtotal: $%d" %subTotal)
-    print("Total con IVA: $%d" %total)
-    print("IVA: $%d" %iva, end="\n\n")
-
-if total > 5000000:
-    total -= subTotal * 0.10
-    print("El total de la factura con un descuento del 10 porciento es: $%d" %total)
-elif total > 2000000:
-    total -= subTotal * 0.03
-    print("El total de la factura con un descuento del 3 porciento es: $%d" %total)
-elif total > 1000000:
-    total -= subTotal * 0.01
-    print("El total de la factura con un descuento del 1 porciento es: $%d" %total)
-else:
-    total = total
-    print("El total de la factura es: $%d" %total)
+    return total
 
 
+def run():
+    subTotal = 0
+    i = 0
+    productos = int(input("Cuantos productos va a facturar: "))
+
+    while i < productos:
+        nombre = input("Nombre del articulo: ")
+        precio = float(input("Precio del articulo sin IVA: "))
+        cantidad = int(input("Cantidad: "))
+        porcentaje = float(input("Porcentaje del IVA: "))
+        subtotal = precio * cantidad
+        subTotal += subtotal
+        iva = subTotal * porcentaje
+        total = subTotal + iva
+        i += 1
+        print("El articulo actual es: %s" %nombre)
+        print("Subtotal: $%d" %subTotal)
+        print("Total con IVA: $%d" %total)
+        print("IVA: $%d" %iva, end="\n\n")
+    
+    if total > 5000000:
+        total -= subTotal * 0.10
+        print("El total de la factura con un descuento del 10 porciento es: $%d" %total)
+    elif total > 2000000:
+        total -= subTotal * 0.03
+        print("El total de la factura con un descuento del 3 porciento es: $%d" %total)
+    elif total > 1000000:
+        total -= subTotal * 0.01
+        print("El total de la factura con un descuento del 1 porciento es: $%d" %total)
+    else:
+        total = total
+        print("El total de la factura es: $%d" %total)
+    
+    
 
 
+if __name__ == "__main__":
+    run()
+   
 
-
-
+ 
