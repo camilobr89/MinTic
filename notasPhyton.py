078902c8dd86747595c063c172bac0e249322494
camilobr89 <camilobr89@hotmail.com>
2022-05-13 19:31:53 -0500

    repaso

--- notasPhyton.py
+++ notasPhyton.py
@@ -1,1 +1,6 @@
 
+"""
+# Palabras reservadas para python:
+and, as, assert, break, class, continue, def, del, elif, else, except, exec, finally, for, from, global, if, import, in, is, lambda, not, or, pass, print, raise, return, try, while, with, yield
+
+"""
