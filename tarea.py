a9cadbe2424383bc1afbf8899b082953687bb841
camilobr89 <camilobr89@hotmail.com>
2022-05-21 18:03:55 -0500

    tarea

--- tarea.py
+++ tarea.py
@@ -12,7 +12,7 @@ por último una tercera función que va asacar la factura con todos los artícul
 
 """
 
-import json
+
 
 articulos = {}
 
@@ -21,7 +21,7 @@ def capturar_articulos():
     codigo = input('Ingrese el codigo del articulo: ')
     articulos[codigo] = {}
     articulos[codigo]['nombre'] = input('Ingrese el nombre del articulo: ')
-    articulos[codigo]['precio'] = float(input('Ingrese el precio unitario con IVA: '))
+    articulos[codigo]['precio con IVA'] = float(input('Ingrese el precio unitario con IVA: '))
     articulos[codigo]['iva'] = float(input('Ingrese el porcentaje del IVA: '))
 
 def facturar():
@@ -31,9 +31,12 @@ def facturar():
     if articulos[codigo]['cantidad'] < 0:
         print('La cantidad no puede ser negativa')
         return
-    articulos[codigo]['subtotal'] = articulos[codigo]['cantidad'] * articulos[codigo]['precio']
-    articulos[codigo]['iva'] = articulos[codigo]['subtotal'] * articulos[codigo]['iva'] / 100
-    articulos[codigo]['total'] = articulos[codigo]['subtotal'] + articulos[codigo]['iva']
+    
+    articulos[codigo]['subtotal sin IVA'] = articulos[codigo]['precio con IVA'] / (articulos[codigo]['iva'] / 100 + 1) * articulos[codigo]['cantidad']
+    articulos[codigo]['subtotal con IVA'] = articulos[codigo]['cantidad'] * articulos[codigo]['precio con IVA']
+    articulos[codigo]['iva'] = articulos[codigo]['subtotal sin IVA'] * articulos[codigo]['iva'] / 100
+    # articulos[codigo]['iva'] = articulos[codigo]['subtotal con IVA'] * articulos[codigo]['iva'] / 100
+    articulos[codigo]['total'] = articulos[codigo]['subtotal con IVA'] + articulos[codigo]['iva']
     
 
 def mostrar_factura():
@@ -42,9 +45,13 @@ def mostrar_factura():
     for articulo in articulos:
         print('Articulo: ', articulos[articulo]['nombre'])
         print('Cantidad: ', articulos[articulo]['cantidad'])
-        print('Subtotal: ', articulos[articulo]['subtotal'])
+        print('Subtotal sin IVA: ', articulos[articulo]['subtotal sin IVA'])
+        print('Subtotal con IVA: ', articulos[articulo]['subtotal con IVA'])
         print('IVA: ', articulos[articulo]['iva'])
         print('Total: ', articulos[articulo]['total'])
+        print('\n')
+        print('-----------------------------------------------------')
+        
  
 
 def run():
@@ -59,19 +66,24 @@ def run():
         print('1. Capturar articulos')
         print('2. Facturar')
         print('3. Mostrar factura')
-        print('4. Salir')
+        print('4. Salir\n')
+        print('-----------------------------------------------------')
         opcion = input('Ingrese una opción: ')
+        print('-----------------------------------------------------')
+        print('\n')
         if opcion == "1":
             capturar_articulos()
         elif opcion == "2":
             facturar()
         elif opcion == "3":
-            print(articulos)
+            mostrar_factura()
         elif opcion == "4":
             break
         else:
             print('Opción no valida')
     
-
+print('-----------------------------------------------------')
 print('Bienvenido al programa de facturación')
+print('-----------------------------------------------------')
+print('\n')
 run()
